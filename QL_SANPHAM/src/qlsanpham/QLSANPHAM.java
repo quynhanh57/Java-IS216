c40bb785723dbb2057de2c32c6fc7b650d01b398
quynhanh57 <90370210+quynhanh57@users.noreply.github.com>
2022-06-25 12:37:21 +0700

    Add files via upload

---
+++ QL_SANPHAM/src/qlsanpham/QLSANPHAM.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package qlsanpham;
+
+/**
+ *
+ * @author Tran Nguyen Quynh Anh
+ */
+public class QLSANPHAM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
