183b1efbba2f2e5c23340d7679f9bb0edde73846
quynhanh57 <90370210+quynhanh57@users.noreply.github.com>
2022-06-25 12:54:57 +0700

    Add files via upload

---
+++ BaiTapThuHanh4/src/baitapthuhanh4/BaiTapThuHanh4.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package baitapthuhanh4;
+
+/**
+ *
+ * @author Admin
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+        
+public class BaiTapThuHanh4 {  
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
